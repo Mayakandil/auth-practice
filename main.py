1cb20817cc6cd5f92fe12a3c017aeefe323ba163
MayaKandil <Kandilmaya04@gmail.com>
2026-09-19 15:12:02 +0300

    Stage 3: profile route token verification

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os # py needs oss to read eviroment variables 
 from dotenv import load_dotenv # to read .env
-from fastapi import FastAPI ,Body , Header# for app = fastapi()
+from fastapi import FastAPI ,Body , Header , Query , Depends , HTTPException# for app = fastapi()
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder # as supabase return non json objects so we encode it to be able to jsonfy it 
 from supabase import create_client , Client # Create_client-> fun conncetion/client betweeen python w supabase  , Client --> type used to indecate that the var supabase is a Supabase Client 
@@ -20,6 +20,35 @@ supabase: Client = create_client(SUPABASE_URL , SUPABASE_KEY) #create a supabase
 
 #create fastapi app
 app = FastAPI()
+
+#dependecies
+async def verify_user(authorization = Header(None)):
+    if not authorization:
+        raise HTTPException(
+            status_code=401,
+            content={"error": "Access token required"}
+        )
+    if not authorization.startswith("Bearer "):
+            raise HTTPException(status_code=401 , content={"error":"Access token required "})
+    
+    token = authorization.removeprefix("Bearer ").strip()
+    
+    if not token:
+          raise HTTPException(status_code=401,content={"error": "Access token required"})
+    
+    
+    try:
+            response = supabase.auth.get_user(token)
+    
+    except Exception:
+            raise HTTPException(status_code=401, content={"error":"Unautherized"})
+
+    return response.user
+    
+    
+
+
+
 
 #home route
 @app.get("/")
@@ -68,19 +97,140 @@ async def public_info():
     return {"message":"welcom starnger! this info is public "}
 
 @app.get("/protected/profile")
-async def protected_profile(authorization = Header(None)):
+async def protected_profile(user = Depends(verify_user)):
+    return {
+        "id":user.id
+        , "email":user.email,
+        "created_at":user.created_at
+    }
 
-    if not authorization :
-        return JSONResponse(status_code=401 , content={"error":"access token is required"})
+@app.get("/protected/dashboard")
+async def protected_dashboard(user = Depends(verify_user)):
 
-    if not authorization.startswith("Bearer "):
-        return JSONResponse(status_code=401 , content={"error":"access token must start with Bearer "})
-
-    text = authorization.split()
-    if not text[1]:
-         return JSONResponse(status_code=401 , content={"error":"access token is required"})
-
-    token = text[1]
-    return "token :",token 
+    return {
+        "message": "Welcome to your dashboard",
+        "user_id": user.id,
+        "email": user.email
+    }
 
 
+# @app.post("/auth/reset-password")
+# async def reset_password(data: dict = Body(...)):
+
+#     email = data.get("email")
+
+#     # Validate input
+#     if not email:
+#         return JSONResponse(
+#             status_code=400,
+#             content={"error": "email is required"}
+#         )
+ 
+#     try:
+#         supabase.auth.reset_password_for_email(email, {
+#                 "redirect_to": "http://localhost:3000/auth/reset-password/confirm"})
+
+#         return JSONResponse(
+#             status_code=200,
+#             content={"message": "password reset email sent"}
+#         )
+
+#     except Exception as e:
+#         return JSONResponse(
+#             status_code=400,
+#             content={"error": str(e)}
+#         )
+
+# # ==========================================
+# # 2. CONFIRM RESET LINK
+# # ==========================================
+
+# @app.get("/auth/reset-password/confirm")
+# async def confirm_reset_password(code: str = Query(None)):
+
+#     if not code:
+#         return JSONResponse(
+#             status_code=400,
+#             content={"error": "recovery code is required"}
+#         )
+
+#     try:
+#         response = supabase.auth.exchange_code_for_session(
+#             {
+#                 "auth_code": code
+#             }
+#         )
+
+#         if not response.session:
+#             return JSONResponse(
+#                 status_code=401,
+#                 content={"error": "invalid or expired recovery code"}
+#             )
+
+#         return JSONResponse(
+#             status_code=200,
+#             content={
+#                 "message": "recovery link verified",
+#                 "access_token": response.session.access_token,
+#                 "refresh_token": response.session.refresh_token
+#             }
+#         )
+
+#     except Exception as e:
+#         return JSONResponse(
+#             status_code=400,
+#             content={"error": str(e)}
+#         )
+
+
+# # ==========================================
+# # 3. SET NEW PASSWORD
+# # ==========================================
+
+# @app.post("/auth/reset-password/new")
+# async def set_new_password(data: dict = Body(...)):
+
+#     new_password = data.get("new_password")
+#     access_token = data.get("access_token")
+#     refresh_token = data.get("refresh_token")
+
+#     # Validate input
+#     if not new_password:
+#         return JSONResponse(
+#             status_code=400,
+#             content={"error": "new password is required"}
+#         )
+
+#     if not access_token or not refresh_token:
+#         return JSONResponse(
+#             status_code=401,
+#             content={"error": "recovery session is required"}
+#         )
+
+#     try:
+
+#         # Set the user's recovery session
+#         supabase.auth.set_session(
+#             access_token,
+#             refresh_token
+#         )
+
+#         # Change password
+#         supabase.auth.update_user(
+#             {
+#                 "password": new_password
+#             }
+#         )
+
+#         return JSONResponse(
+#             status_code=200,
+#             content={
+#                 "message": "password updated successfully"
+#             }
+#         )
+
+#     except Exception as e:
+#         return JSONResponse(
+#             status_code=400,
+#             content={"error": str(e)}
+#         )
